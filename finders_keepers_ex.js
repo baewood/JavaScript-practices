86065b0516a0f2b2e13c0b6b2082653a2870c774
baewood <baewood@gmail.com>
2019-10-28 10:11:27 -0500

    no message

---
+++ finders_keepers_ex.js
@@ -0,0 +1,16 @@
+// Finders Keepers, Free Code Camp Exercise
+// Using a for loop
+
+function findElement(arr, func) {
+  let num = 0;
+
+  for (var i = 0; i < arr.length; i++){
+    num = arr[i];
+    if(func(num)){
+      return num;
+    }
+  }
+  return undefined;
+}
+
+findElement([1, 2, 3, 4], num => num % 2 === 0);
